e61a648ed58dc5c5bd2b50bffa1696cf3f50dbd9
Bilal H <hussain.bilal000786@gmail.com>
2018-03-30 18:04:03 +0530

    Add splash image

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ import { StackNavigator } from 'react-navigation';
 
 import axios from 'axios';
 
-import { BarCodeScanner, Permissions } from 'expo';
+import { Asset, AppLoading } from 'expo';
 
 import TextField from './components/TextField';
 import AnimatedScanner from './components/AnimatedScanner';
@@ -44,7 +44,35 @@ const RootStack = StackNavigator(
 );
 
 export default class App extends React.Component {
+
+  state = {
+    isReady: false
+  };
+
   render() {
+    if(!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}/>
+      )
+    }
+
     return <RootStack />;
+  }
+
+  async _cacheResourcesAsync() {
+    const images = [
+      require('./Logo_full.png'),
+      require('./Or.png'),
+      require('./logoBottom.png'),
+    ];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    })
+
+    return Promise.all(cacheImages)
   }
 }
